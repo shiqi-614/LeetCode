bc3e8d210bd20becd661f254e9db261e3fb1d235
shiqi_614 <shiqi614@gmail.com>
2021-02-13 16:03:26 +0800

    321 done

--- 321.cpp
+++ 321.cpp
@@ -32,11 +32,6 @@ typedef vector<double> VD;
 typedef long long LL;
 typedef pair<int, int> PII;
 
-#define MID(lft, rht) (lft + ((rht - lft) >> 1))
-#define LL(x) (x << 1)
-#define RR(x) ((x << 1) | 1)
-
-const int N = 1e5 + 5;
 struct Node {
     int pos;
     int val;
@@ -48,90 +43,91 @@ struct Node {
     }
     bool operator<(const Node& B) const
     {
-        return val > B.val;
-    }
-};
-struct SegTree {
-    Node mx[N * 4];
-    void build(int lft, int rht, int idx, vector<int>& data)
-    {
-        if (lft == rht) {
-            mx[idx] = Node(lft, data[lft]);
-        } else {
-            int mid = MID(lft, rht);
-            build(lft, mid, LL(idx), data);
-            build(mid + 1, rht, RR(idx), data);
-            if (mx[LL(idx)].val > mx[RR(idx)].val) {
-                mx[idx] = mx[LL(idx)];
-            } else {
-                mx[idx] = mx[RR(idx)];
-            }
-        }
-    }
-    Node query(int lft, int rht, int idx, int st, int ed)
-    {
-        if (st <= lft && rht <= ed) {
-            return mx[idx];
-        } else {
-            int mid = MID(lft, rht);
-            Node mx1 = Node(-1, -1);
-            Node mx2 = Node(-1, -1);
-            if (st <= mid)
-                mx1 = query(lft, mid, LL(idx), st, ed);
-            if (ed > mid)
-                mx2 = query(mid + 1, rht, RR(idx), st, ed);
-            if (mx1.val > mx2.val) {
-                return mx1;
-            } else {
-                return mx2;
-            }
-        }
+        return val < B.val;
     }
 };
 
+bool sortByPos(const Node& a, const Node& b)
+{
+    return a.pos < b.pos;
+}
+void printVector(vector<Node>& data)
+{
+
+    for (int i = 0; i < (int)data.size(); i++) {
+        cout << data[i].val << " ";
+    }
+    cout << endl;
+}
+void print(vector<Node>& order1, vector<Node>& order2, vector<int>& res)
+{
+    cout << "order1:" << endl;
+    printVector(order1);
+    cout << "order2:" << endl;
+    printVector(order2);
+    cout << "res::" << endl;
+    for (int i = 0; i < (int)res.size(); i++)
+        cout << res[i] << " ";
+    cout << endl;
+}
 class Solution {
 public:
-    SegTree seg1;
-    SegTree seg2;
     int len1;
     int len2;
-    int K;
-    vector<int> nums1;
-    vector<int> nums2;
-    vector<int> merge(vector<Node>& order1, vector<Node>& order2)
+    vector<int> merge(vector<Node>& order1, vector<Node>& order2, int K)
     {
         int idx1 = 0;
         int idx2 = 0;
         vector<int> res;
-        while (idx1 < (int)order1.size() || idx2 < (int)order2.size()) {
+        while (1) {
+            if (idx1 >= (int)order1.size() && idx2 >= (int)order2.size()) {
+                break;
+            }
+            if (K <= 0) {
+                break;
+            }
+
             if (idx1 < (int)order1.size() && idx2 < (int)order2.size()) {
                 if (order1[idx1].val > order2[idx2].val) {
                     res.push_back(order1[idx1].val);
                     idx1++;
+                    K--;
                 } else if (order1[idx1].val < order2[idx2].val) {
                     res.push_back(order2[idx2].val);
                     idx2++;
+                    K--;
                 } else {
                     if (compareUntilDifference(order1, order2, idx1, idx2)) {
                         res.push_back(order1[idx1].val);
                         idx1++;
+                        K--;
                     } else {
-                        res.push_back(order1[idx2].val);
+                        res.push_back(order2[idx2].val);
                         idx2++;
+                        K--;
                     }
                 }
             } else if (idx1 < (int)order1.size()) {
                 res.push_back(order1[idx1].val);
                 idx1++;
-
+                K--;
             } else {
-                res.push_back(order1[idx2].val);
+                res.push_back(order2[idx2].val);
                 idx2++;
+                K--;
             }
         }
         return res;
     }
 
+    string getStr(vector<int>& data)
+    {
+        string res = "";
+        for (int i = 0; i < (int)data.size(); i++) {
+            res += (data[i] + '0');
+        }
+        return res;
+    }
     bool compareUntilDifference(vector<Node>& order1, vector<Node>& order2, int idx1, int idx2)
     {
         while (idx1 < (int)order1.size()
@@ -147,120 +143,137 @@ public:
             return true;
         return order1[idx1].val > order2[idx2].val;
     }
-    void dfs(int lft1, int rht1, int lft2, int rht2, vector<Node>& order1, vector<Node>& order2)
-    {
-        cout << "lft1:" << lft1 << " rht1:" << rht1 << " lft2:" << lft2 << " rht2:" << rht2 << " K:" << this->K << endl;
-        if (K < 0)
-            return;
-        if (lft1 > rht1 && lft2 > rht1) {
-            return;
-        }
 
-        Node mx1 = Node(-1, -1);
-        Node mx2 = Node(-1, -1);
-
-        if (lft1 <= rht1) {
-            mx1 = seg1.query(0, len1 - 1, 1, lft1, rht1);
-        }
-        if (lft2 <= rht2) {
-            mx2 = seg2.query(0, len2 - 1, 1, lft2, rht2);
-        }
-        K--;
-        if (mx1.val > mx2.val) {
-            order1.push_back(mx1);
-            dfs(mx1.pos + 1, rht1, lft2, rht2, order1, order2);
-        } else if (mx1.val <= mx2.val) {
-            order2.push_back(mx2);
-            dfs(lft1, rht1, mx2.pos + 1, rht2, order1, order2);
-        }
-    }
-
-    int getPrePos(int idx, vector<Node> order)
-    {
-        if (idx - 1 >= 0) {
-            return order[idx - 1].pos + 1;
-        } else {
-            return 0;
-        }
-    }
-    vector<Node> getData(int lft, int rht, vector<int> nums)
+    bool getList(vector<Node>& order, vector<int>& nums, int remain, vector<Node>& list1)
     {
         priority_queue<Node> que;
-        for (int i = lft; i <= rht; i++) {
-            que.push(Node(i, nums[i]));
+        for (int i = (int)order.size() - 1; i >= 0; i--) {
+            int ed = order[i].pos - 1;
+            int st = getSt(order, i - 1);
+            list1.push_back(order[i]);
+
+            for (int j = ed; j >= st; j--) {
+                que.push(Node(j, nums[j]));
+            }
+            while (!que.empty() && remain) {
+                list1.push_back(que.top());
+                que.pop();
+                remain--;
+            }
         }
+        sort(list1.begin(), list1.end(), sortByPos);
+        if (remain == 0)
+            return true;
+        return false;
+    }
+
+    int getSt(vector<Node>& order, int idx)
+    {
+        if (idx < 0)
+            return 0;
+        else
+            return order[idx].pos + 1;
+    }
+
+    vector<Node> getMaxSeq(int lft, int rht, vector<int>& nums, int cnt)
+    {
+        //cout << "get max seq:" << lft << " rht:" << rht << " cnt:" << cnt << endl;
+        int remain = cnt;
         vector<Node> data;
-        while (!que.empty() && K--) {
-            data.push_back(que.top());
-            que.pop();
+        int len = (int)nums.size();
+        if (len == 0)
+            return data;
+        vector<int> pos(len, -1);
+        int mx = -1;
+        int mxPos = -1;
+        for (int i = rht; i >= lft; i--) {
+            if (mx <= nums[i]) {
+                mx = nums[i];
+                mxPos = i;
+            }
+            pos[i] = mxPos;
         }
+        int idx = pos[lft];
+        while (idx <= rht && cnt) {
+            data.push_back(Node(idx, nums[idx]));
+            if (idx + 1 <= rht) {
+                idx = pos[idx + 1];
+            } else {
+                break;
+            }
+            cnt--;
+            //cout << "idx:" << idx << " val:" << nums[idx] << endl;
+        }
+        //cout << "lft:" << lft << " rht:" << rht << " remain:" << remain << " Max Seq:" << endl;
+        //for (int i = 0; i < (int)data.size(); i++)
+        //cout << data[i].val << " ";
+        //cout << endl;
         return data;
     }
-    void mergeOrder(vector<Node>& newData, vector<Node>& data, int idx)
+
+    vector<Node> getSeq(int lft, int rht, vector<int>& nums, int cnt)
     {
-        vector<Node> tmp(data.begin(), data.end());
-        data.clear();
-        for (int i = (int)tmp.size() - 1; i >= 0; i--) {
-            data.push_back(tmp[i]);
-            if (i == idx) {
-                for (int j = 0; j < (int)newData.size(); j++) {
-                    data.push_back(newData[j]);
+        //cout << "get seq:" << lft << " rht:" << rht << " cnt:" << cnt << endl;
+        vector<Node> data;
+        if (cnt <= 0 || lft > rht)
+            return data;
+        if (rht - lft + 1 <= cnt) {
+            for (int i = lft; i <= rht; i++) {
+                data.push_back(Node(i, nums[i]));
+            }
+        } else {
+            data = getMaxSeq(lft, rht, nums, cnt);
+            cnt -= (int)data.size();
+
+            if (cnt <= 0) {
+                sort(data.begin(), data.end(), sortByPos);
+                return data;
+            }
+            vector<Node> tmp;
+            for (int i = (int)data.size() - 1; i >= 0; i--) {
+                tmp = getSeq(data[i].pos + 1, i + 1 <= (int)data.size() - 1 ? data[i + 1].pos - 1 : rht, nums, cnt);
+                cnt -= (int)tmp.size();
+                for (int j = 0; j < (int)tmp.size(); j++) {
+                    data.push_back(tmp[j]);
+                }
+                if (cnt <= 0) {
+                    sort(data.begin(), data.end(), sortByPos);
+                    return data;
                 }
             }
-        }
-        reverse(data.begin(), data.end());
-    }
-    void dfs2(int idx1, int idx2, vector<Node>& order1, vector<Node>& order2)
-    {
-        if (K <= 0) {
-            return;
-        }
-        if (idx1 >= 0 && idx2 >= 0) {
-            if (order1[idx1].val < order2[idx2].val) {
-                vector<Node> pos = getData(getPrePos(idx1, order1), order1[idx1].pos - 1, nums1);
-                mergeOrder(pos, order1, idx1);
-                idx1--;
-            } else if (order1[idx1].val > order2[idx2].val) {
-                vector<Node> pos = getData(getPrePos(idx2, order2), order2[idx2].pos - 1, nums2);
-                mergeOrder(pos, order2, idx2);
-                idx2--;
-            } else {
-                vector<Node> subOrder1;
-                vector<Node> subOrder2;
-                dfs(getPrePos(idx1, order1), order1[idx1].pos - 1, getPrePos(idx2, order2), order2[idx2].pos - 1, subOrder1, subOrder2);
-                mergeOrder(subOrder1, order1, idx1);
-                mergeOrder(subOrder2, order2, idx2);
-                idx1--;
-                idx2--;
+            tmp = getSeq(lft, data[0].pos - 1, nums, cnt);
+            for (int j = 0; j < (int)tmp.size(); j++) {
+                data.push_back(tmp[j]);
             }
-        } else if (idx1 >= 0) {
-            vector<Node> pos = getData(getPrePos(idx1, order1), order1[idx1].pos - 1, nums1);
-            mergeOrder(pos, order1, idx1);
-            idx1--;
-        } else if (idx2 >= 0) {
-            vector<Node> pos = getData(getPrePos(idx2, order2), order2[idx2].pos - 1, nums2);
-            mergeOrder(pos, order2, idx2);
+            cnt -= (int)tmp.size();
         }
+        sort(data.begin(), data.end(), sortByPos);
+        return data;
     }
 
     vector<int> maxNumber(vector<int>& nums1, vector<int>& nums2, int k)
     {
-        len1 = (int)nums1.size();
-        len2 = (int)nums2.size();
-        this->nums1 = nums1;
-        this->nums2 = nums2;
-        seg1.build(0, len1 - 1, 1, nums1);
-        seg2.build(0, len2 - 1, 1, nums2);
-        this->K = k;
+        int K = k;
+        string resStr = "";
+        vector<int> res;
+        for (int i = 0; i <= K; i++) {
+            //cout << "K:" << K << " order1 cnt:" << i << ", order2 cnt:" << k - i << endl;
+            vector<Node> order1 = getSeq(0, (int)nums1.size() - 1, nums1, i);
+            vector<Node> order2 = getSeq(0, (int)nums2.size() - 1, nums2, K - i);
 
-        vector<Node> order1;
-        vector<Node> order2;
-        dfs(0, len1 - 1, 0, len2 - 1, order1, order2);
-        if (K > 0) {
-            dfs2((int)order1.size() - 1, (int)order2.size() - 1, order1, order2);
+            bool flag1 = ((int)order1.size() >= i);
+            bool flag2 = ((int)order2.size() >= (K - i));
+            if (flag1 && flag2) {
+                vector<int> tmp = merge(order1, order2, K);
+                string tmpStr = getStr(tmp);
+                if (tmpStr > resStr) {
+                    resStr = tmpStr;
+                    res = tmp;
+                }
+                //print(order1, order2, res);
+            }
         }
-
-        return merge(order1, order2);
+        return res;
     }
 };
 
@@ -270,18 +283,34 @@ int main()
     vector<int> data1;
     vector<int> data2;
 
-    int tmp1[] = { 3, 4, 6, 5 };
-    int tmp2[] = { 9, 1, 2, 5, 8, 3 };
-
+    //int tmp1[] = { 3, 9 };
+    //int tmp2[] = { 8, 9 };
+    //int tmp1[] = { 5, 5, 1 };
+    //int tmp2[] = { 4, 0, 1 };
+    //int tmp1[] = { 2, 2, 0, 6, 8, 9, 6 };
+    //int tmp2[] = { 5, 2, 4, 5, 3, 6, 2 };
+    //int tmp1[] = { 8, 0, 4, 4, 1, 7, 3, 6, 5, 9, 3, 6, 6, 0, 2, 5, 1, 7, 7, 7, 8, 7, 1, 4, 4, 5, 4, 8, 7, 6, 2, 2, 9, 4, 7, 5, 6, 2, 2, 8, 4, 6, 0, 4, 7, 8, 9, 1, 7, 0 };
+    //int tmp2[] = { 6, 9, 8, 1, 1, 5, 7, 3, 1, 3, 3, 4, 9, 2, 8, 0, 6, 9, 3, 3, 7, 8, 3, 4, 2, 4, 7, 4, 5, 7, 7, 2, 5, 6, 3, 6, 7, 0, 3, 5, 3, 2, 8, 1, 6, 6, 1, 0, 8, 4 };
+    //int tmp1[] = { 3, 4, 6, 5 };
+    //int tmp2[] = { 9, 1, 2, 5, 8, 3 };
+    //int tmp1[] = { 6, 7 };
+    //int tmp2[] = { 6, 0, 4 };
+    int tmp2[] = {};
+    int tmp1[] = { 1, 2 };
+    cout << "len1:" << (int)sizeof(tmp1) / sizeof(int) << endl;
     for (int i = 0; i < (int)sizeof(tmp1) / sizeof(int); i++) {
         data1.push_back(tmp1[i]);
     }
 
+    cout << "len2:" << (int)sizeof(tmp2) / sizeof(int) << endl;
     for (int i = 0; i < (int)sizeof(tmp2) / sizeof(int); i++) {
         data2.push_back(tmp2[i]);
     }
 
-    vector<int> res = s.maxNumber(data1, data2, 5);
+    //vector<int> res = s.maxNumber(data1, data2, 5);
+    //vector<int> res = s.maxNumber(data1, data2, 3);
+    //vector<int> res = s.maxNumber(data1, data2, 5);
+    vector<int> res = s.maxNumber(data1, data2, 2);
     for (int i = 0; i < (int)res.size(); i++) {
         cout << res[i] << " ";
     }
