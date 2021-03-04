d5f4ae4e73f58124fa7496d6aa2ca7e519ca2a25
cai, jinsong <shiqi_614@qq.com>
2021-03-05 00:33:58 +0800

    483 done

--- 483.java
+++ 483.java
@@ -1,16 +1,17 @@
 import java.math.BigInteger;
 class Solution {
-    String getRes(int cnt) {
+    String getRes(int cnt)
+    {
         String res = "";
         for (int i = 0; i < cnt; i++) {
-            res +="1";
+            res += "1";
         }
         return res;
     }
 
-    BigInteger checkVal(int len, int intBase) {
-        BigInteger val = new BigInteger("0");    
-        BigInteger base = new BigInteger(String.valueOf(intBase));
+    BigInteger checkVal(int len, BigInteger base)
+    {
+        BigInteger val = new BigInteger("0");
         BigInteger mul = new BigInteger("1");
         for (int i = 0; i < len; i++) {
             val = val.add(mul);
@@ -19,32 +20,61 @@ class Solution {
         return val;
     }
 
-    boolean check(int len, BigInteger val) {
-        int lft = 2;
-        int rht = val -1;
-        while(lft < rht) {
-            int mid = (lft + (rht - lft) / 2);
+    BigInteger check(int len, BigInteger val)
+    {
+        BigInteger lft = BigInteger.TWO;
+        BigInteger rht = val;
+        while (true) {
+            if (lft.compareTo(rht) >= 0)
+                break;
+            BigInteger sum = lft.add(rht);
+            BigInteger mid = sum.divide(BigInteger.TWO);
             BigInteger tmp = checkVal(len, mid);
             int num = tmp.compareTo(val);
+            System.out.println("lft:" + lft + " mid:" + mid + " rht:" + rht);
             if (num > 0) {
-                rht = mid - 1;
+                rht = mid.subtract(BigInteger.ONE);
             } else if (num == 0) {
-                return true;
+                return mid;
             } else if (num < 0) {
-                lft = mid + 1;
+                lft = mid.add(BigInteger.ONE);
             }
         }
-        return false;
+        if (lft.compareTo(rht) == 0 ) {
+            BigInteger tmp = checkVal(len, lft);
+            if (tmp.compareTo(val) == 0) {
+                return lft;
+            }
+        }
+        return new BigInteger("-1");
     }
 
-    public String smallestGoodBase(String n) { 
+    int getMaxLen(BigInteger val)
+    {
+        BigInteger tmp = val;
+        int cnt = 0;
+        while (true) {
+            if (tmp.compareTo(BigInteger.ZERO) <= 0) {
+                break;
+            }
+            cnt++;
+            tmp = tmp.divide(BigInteger.TWO);
+        }
+        return cnt;
+    }
+    public String smallestGoodBase(String n)
+    {
         BigInteger val = new BigInteger(n);
         int maxLen = getMaxLen(val);
         int minLen = 2;
-        for (int i = maxLen; i >= minLen; i++) {
-            if (check(i, val)) {
-                return getRes(i);
+        BigInteger invalid = new BigInteger("-1");
+        for (int i = maxLen; i >= minLen; i--) {
+            BigInteger res = check(i, val);
+            System.out.println("len:" + i + " " + res);
+            if (invalid.compareTo(res) != 0) {
+                return res.toString();
             }
         }
+        return n;
     }
 }
